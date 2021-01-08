7d3bc4eb745ec7880c407db988ce9f4102bc4480
liudan <liudan@growingio.com>
2021-01-08 15:32:30 +0800

    fix: filter-picker storybook组件样式补充

--- src/filter-picker/FilterPicker.stories.tsx
+++ src/filter-picker/FilterPicker.stories.tsx
@@ -4,7 +4,12 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 import FilterPicker from '.';
 import { FilterPickerProps } from './interfaces';
 // import { currentUserId, preparedSegments, segments } from './__test__/data';
+
 import '@gio-design-new/components/es/components/button/style/css';
+import '@gio-design-new/components/es/components/select/style/css';
+import '@gio-design-new/components/es/components/date-picker/style/css';
+import '@gio-design-new/components/es/components/checkbox/style/css';
+import '@gio-design-new/components/es/components/list/style/css';
 import './style/index.less';
 
 export default {
@@ -13,6 +18,48 @@ export default {
 } as Meta;
 
 const Template: Story<FilterPickerProps> = (args) => <FilterPicker {...args} />;
+
+const dimissionValue = [
+  'www.growingio.com',
+  'N/A',
+  'accounts.growingio.com',
+  'wx51cba5e78d4ef4d8',
+  'docs.growingio.com',
+  'demo1.growingio.com',
+  'wx9d29e4385408f222',
+  'www.test.sdk.com',
+  'www.meiyu.sdk.com',
+  'www.xiaomei.sdk.com',
+  'growingio.kf5.com',
+  'demo.growingio.com',
+  'messages.growingio.com',
+  'help.growingio.com',
+  'growing.kf5.com',
+  'release-www.growingio.cn',
+  'www.zeding.net',
+  'fep0.growingio.com',
+  'liu-huaqing.gitbooks.io',
+  '117.50.63.183',
+  'sishen.gitbooks.io',
+  'com.zqm.test0219',
+  'release-deeplink.growingio.cn',
+  'www.huanqiujindun.com',
+  'www.xinonggaoke.com',
+  'com.growingio.gtouch',
+  'www.mljsj.net',
+  'www.kinglysoft.com',
+  'dev.lyajt.com',
+  'release-api.growingio.cn:9091',
+  'translate.googleusercontent.com',
+  'www.schoolso.net',
+  'gio.jx139.com',
+  '39.106.230.138',
+  '10.20.20.23:8086',
+  '39.103.31.137',
+  'zs.ttylink.com',
+  'erp.ttylink.com',
+  '10.20.20.23:8080',
+];
 
 export const Default = Template.bind({});
 Default.args = {
@@ -382,4 +429,8 @@ Default.args = {
   recentlyStorePrefix: 'currentUserId',
   timeRange: 'day:31,1',
   measurements: [{ id: 'vv', type: 'prepared', attribute: 'count', __typename: 'Measurement265f' }],
+  dimensionValueRequest: () =>
+    new Promise((resolve) => {
+      resolve(dimissionValue);
+    }),
 };
