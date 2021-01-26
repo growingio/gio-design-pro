8d3d9a010c24603cb0e5affcd4b188fbf2991b2e
maxin <maxin@growingio.com>
2021-01-26 20:07:14 +0800

    test(filter-picker): 添加数字类型的单测

--- src/filter-picker/__test__/FilterPicker.test.js
+++ src/filter-picker/__test__/FilterPicker.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount, render } from 'enzyme';
-import { act } from 'react-dom/test-utils';
 import FilterPicker from '..';
 import { sleep } from './utils.ts';
 
@@ -26,173 +25,78 @@ afterEach(() => {
 
 describe('FilterPicker', () => {
   // 基础渲染
-  describe('snapshot', () => {
-    const getFilterPicker = (props) => (
-      <FilterPicker
-        propertyOptions={[
-          {
-            id: 'test_int_id',
-            name: 'test_int_name',
-            groupId: 'user',
-            groupName: '用户属性',
-            type: 'usr',
-            valueType: 'int',
-            __typename: 'Dimension',
-          },
-        ]}
-        filter={{ op: 'and', exprs: [], __typename: 'directivesFilter' }}
-        {...props}
-      />
-    );
+  const getFilterPicker = (props) => (
+    <FilterPicker
+      propertyOptions={[
+        {
+          id: 'test_int_id',
+          name: 'test_int_name',
+          groupId: 'user',
+          groupName: '用户属性',
+          type: 'usr',
+          valueType: 'int',
+          __typename: 'Dimension',
+        },
+      ]}
+      filter={{ op: 'and', exprs: [], __typename: 'directivesFilter' }}
+      {...props}
+    />
+  );
 
-    it('should match FilterPicker base snapshot.', () => {
-      const wrapper = render(getFilterPicker());
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    // 使用 children 时，应该正确显示
-    it('when use children, should render it correctly', () => {
-      const children = <div>过滤选择器</div>;
-      const wrapper = render(getFilterPicker({ children }));
-      expect(wrapper.text()).toEqual('过滤选择器');
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should render correctly', () => {
-      const wrapper = mount(getFilterPicker());
-      // 点击过滤器按钮触发过滤弹窗
-      wrapper.find('button').at(0).simulate('click');
-
-      // 此时这两个按钮应该为 disabled 状态
-      const buttons = wrapper.find('button[disabled]');
-      expect(buttons.at(0).text()).toBe('添加过滤条件');
-      expect(buttons.at(1).text()).toBe('确 认');
-
-      wrapper.find('.gio-picker-trigger').simulate('click');
-      // 正确渲染 1 个属性
-      expect(wrapper.find('.cascader-menu-item-inner')).toHaveLength(1);
-    });
-
-    it('cancel', () => {
-      const wrapper = mount(getFilterPicker());
-      wrapper.find('button').simulate('click');
-      wrapper.find('.gio-dropdown').find('.filter-contidion-footer button').at(0).simulate('click');
-      expect(wrapper.find('.gio-dropdown')).toHaveLength(0);
-    });
+  it('should match FilterPicker base snapshot.', () => {
+    const wrapper = render(getFilterPicker());
+    expect(wrapper).toMatchSnapshot();
   });
 
-  describe('can be selected number type', () => {
-    const getFilterPicker = (props) => (
-      <FilterPicker
-        propertyOptions={[
-          {
-            id: 'test_int_id',
-            name: 'test_int_name',
-            groupId: 'user',
-            groupName: '用户属性',
-            type: 'usr',
-            valueType: 'int',
-            __typename: 'Dimension',
-          },
-          {
-            id: 'test_int_id1',
-            name: 'test_int_name1',
-            groupId: 'user',
-            groupName: '用户属性',
-            type: 'usr',
-            valueType: 'int',
-            __typename: 'Dimension',
-          },
-          {
-            id: 'test_int_id2',
-            name: 'test_int_name2',
-            groupId: 'user',
-            groupName: '用户属性',
-            type: 'usr',
-            valueType: 'int',
-            __typename: 'Dimension',
-          },
-        ]}
-        filter={{ op: 'and', exprs: [], __typename: 'directivesFilter' }}
-        {...props}
-      />
-    );
+  // 使用 children 时，应该正确显示
+  it('when use children, should render it correctly', () => {
+    const children = <div>过滤选择器</div>;
+    const wrapper = render(getFilterPicker({ children }));
+    expect(wrapper.text()).toEqual('过滤选择器');
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    const handle = async (op, values, index) => {
-      const onConfirm = jest.fn();
-      let wrapper = null;
-      wrapper = mount(getFilterPicker({ onConfirm }));
-      act(() => {
-        wrapper.find('button').at(0).simulate('click');
-      });
-      await sleep(300);
-      act(() => {
-        wrapper.find('.gio-picker-trigger').simulate('click');
-      });
+  it('should render correctly', () => {
+    const wrapper = mount(getFilterPicker());
+    // 点击过滤器按钮触发过滤弹窗
+    wrapper.find('button').at(0).simulate('click');
 
-      expect(wrapper.find('.gio-property-picker-trigger').hasClass('open')).toBe(true);
-      expect(wrapper).toMatchSnapshot();
-      act(() => {
-        wrapper.find('Dropdown').at(1).find('.cascader-menu-item-inner').at(0).simulate('click');
-      });
-      wrapper.update();
-      expect(wrapper.find('.gio-picker-trigger').text()).toBe('test_int_name');
-      expect(wrapper.find('.filter-condition_select').exists()).toBe(true);
+    // 此时这两个按钮应该为 disabled 状态
+    const buttons = wrapper.find('button[disabled]');
+    expect(buttons.at(0).text()).toBe('添加过滤条件');
+    expect(buttons.at(1).text()).toBe('确 认');
 
-      act(() => {
-        wrapper.find('.filter-condition_select').simulate('click');
-      });
-      act(() => {
-        wrapper.find('.gio-select').simulate('click');
-      });
+    wrapper.find('.gio-picker-trigger').simulate('click');
+    // 正确渲染 1 个属性
+    expect(wrapper.find('.cascader-menu-item-inner')).toHaveLength(1);
+  });
 
-      act(() => {
-        wrapper.find('.gio-select-dropdown .gio-select-option').at(index).simulate('click');
-      });
-      act(() => {
-        wrapper.find('Dropdown').at(2).find('.filter-contidion-footer').find('button').at(1).simulate('click');
-      });
-      act(() => {
-        wrapper.find('Dropdown').at(0).find('.filter-contidion-footer').find('button').at(1).simulate('click');
-      });
-      expect(onConfirm).toBeCalledWith({
-        __typename: 'directivesFilter',
-        exprs: [{ key: 'test_int_id', name: 'test_int_name', op, valueType: 'int', values }],
-        op: 'and',
-      });
-      act(() => {
-        wrapper.unmount();
-      });
-    };
+  it('cancel', () => {
+    const wrapper = mount(getFilterPicker());
+    wrapper.find('button').simulate('click');
+    wrapper.find('.gio-dropdown').find('.filter-contidion-footer button').at(0).simulate('click');
+    expect(wrapper.find('.gio-dropdown')).toHaveLength(0);
+  });
 
-    it('=', () => {
-      handle('=', ['0'], 0);
-    });
-
-    it('!=', () => {
-      handle('!=', ['0'], 1);
-    });
-
-    it('>', () => {
-      handle('>', ['0'], 2);
-    });
-    it('>=', () => {
-      handle('>=', ['0'], 3);
-    });
-    it('<', () => {
-      handle('<', ['0'], 4);
-    });
-    it('<=', () => {
-      handle('<=', ['0'], 5);
-    });
-    it('between', () => {
-      handle('between', ['0'], 6);
-    });
-    it('exist value', () => {
-      handle('hasValue', [' '], 7);
-    });
-    it('not value', () => {
-      handle('noValue', [' '], 8);
-    });
+  it('onConfirm', async () => {
+    const onConfirm = jest.fn();
+    let wrapper = null;
+    wrapper = mount(getFilterPicker({ onConfirm }));
+    wrapper.find('button').at(0).simulate('click');
+    await sleep(100);
+    wrapper.find('.gio-picker-trigger').simulate('click');
+    expect(wrapper.find('.gio-property-picker-trigger').hasClass('open')).toBe(true);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('Dropdown').at(1).find('.cascader-menu-item-inner').at(0).simulate('click');
+    await sleep(100);
+    wrapper.update();
+    expect(wrapper.find('.gio-picker-trigger').text()).toBe('test_int_name');
+    expect(wrapper.find('.filter-condition_select').exists()).toBe(true);
+    wrapper.find('.filter-condition_select').simulate('click');
+    wrapper.find('.gio-select').simulate('click');
+    wrapper.find('.gio-select-dropdown .gio-select-option').at(1).simulate('click');
+    wrapper.find('Dropdown').at(2).find('.filter-contidion-footer').find('button').at(1).simulate('click');
+    wrapper.find('Dropdown').at(0).find('.filter-contidion-footer').find('button').at(1).simulate('click');
+    expect(onConfirm).toBeCalledTimes(1);
   });
 });
