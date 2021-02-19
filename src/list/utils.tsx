3040ae6bf55dc0a4c4578aa3c889458626face90
haozhigang <haozhigang@outlook.com>
2021-02-19 14:24:55 +0800

    feat(list): 展开全部添加 折叠项的数量

--- src/list/utils.tsx
+++ src/list/utils.tsx
@@ -21,7 +21,13 @@ export function renderExpandableItems(expanded: boolean, currentItems: ListItemP
     const showItems = currentItems.slice(0, DEFAULT_SHOW_ITEMS_COUNT);
     return showItems
       .map(renderItem)
-      .concat(<ExpandItem key={`expand-item-${showItems.length + 1}`} onClick={onExpand} />);
+      .concat(
+        <ExpandItem
+          title={`展开全部 (${currentItems.length - showItems.length})`}
+          key={`expand-item-${showItems.length + 1}`}
+          onClick={onExpand}
+        />
+      );
   }
   return currentItems.map(renderItem);
 }
