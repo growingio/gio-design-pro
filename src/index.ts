7addac5052d40147e687ee21891817efc8b5f643
liudan <liudan@growingio.com>
2021-03-05 13:52:46 +0800

    fix(filterpicker): 修改yarn config registry

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import EmptyPrompt from './empty-prompt';
 import _PropertySelector from './property-selector';
+import _FilterPicker from './filter-picker';
 
 export { default as UserPicker, UserPickerProps } from './user-picker';
 export { default as UserSelector, UserSelectorProps } from './user-selector';
 export const PropertySelector = _PropertySelector;
 
+export const FilterPicker = _FilterPicker;
 export const { PropertyPicker } = _PropertySelector;
 export default { EmptyPrompt };
