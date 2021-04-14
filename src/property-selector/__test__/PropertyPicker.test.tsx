04ce257a870aeaa6e9bf13511e666162992c15c6
haozhigang <haozhigang@outlook.com>
2021-04-14 13:48:45 +0800

    test(property-picker): fix test error

--- src/property-selector/__test__/PropertyPicker.test.tsx
+++ src/property-selector/__test__/PropertyPicker.test.tsx
@@ -68,20 +68,21 @@ describe('PropertyPicker', () => {
     expect(handleSelect).toHaveBeenCalledTimes(1);
     expect(screen.queryAllByText(tobeClickedNode.name)).toHaveLength(1);
   });
-  it('can hover a property and show the detail of property', () => {
-    render(<PropertyPicker {...defaultProps} detailVisibleDelay={1} />);
+  it('can hover a property and show the detail of property', async () => {
+    render(<PropertyPicker {...defaultProps} detailVisibleDelay={0} />);
     fireEvent.click(screen.getByText('全部'));
     const item = screen.queryAllByText(insightDimensions[0].name)[0];
-    act(() => {
+
+    await act(async () => {
       fireEvent.mouseEnter(item);
-      jest.runAllTimers();
+      // jest.runAllTimers();
+      jest.runOnlyPendingTimers();
     });
 
-    setTimeout(() => {
-      expect(screen.queryByText(insightDimensions[0].id)).toBeTruthy();
-    }, 2);
+    expect(screen.queryByText(insightDimensions[0].id)).toBeTruthy();
+
     // screen.debug(screen.queryByText(insightDimensions[0].id));
-    fireEvent.mouseLeave(item);
+    // fireEvent.mouseLeave(item);
   });
 
   it('can search a property by name', () => {
