f748aacef0b8811660a5912db2d6a17d24fb181a
lihang <lihang@growingio.com>
2021-04-15 20:25:50 +0800

    feat(tablecard): add showtabs prop

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,5 @@ export const FilterPicker = _FilterPicker;
 export const { PropertyPicker } = _PropertySelector;
 export default { EmptyPrompt };
 export { TableCard, TableCardProps } from './table-card';
+export { default as TitleInfoCard, TitleInfoCardProps } from './title-info-card';
+export { default as OperationMenu, OperationMenuProps } from './operation-menu';
