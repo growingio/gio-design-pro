bb4024635dd18328351b23318ddb8b50f6b111aa
lihang <lihang@growingio.com>
2021-04-21 17:10:53 +0800

    feat(tablecard): button support tooltip

--- src/table-card/style/index.ts
+++ src/table-card/style/index.ts
@@ -4,3 +4,4 @@ import '@gio-design/components/es/components/tabs/style';
 import '@gio-design/components/es/components/button/style';
 import '@gio-design/components/es/components/search-bar/style';
 import '@gio-design/components/es/components/space/style';
+import '../../tooltip-button/style';
--- src/tooltip-button/style/index.tsx
+++ src/tooltip-button/style/index.tsx
@@ -1,1 +1,3 @@
 import './index.less';
+import '@gio-design/components/es/components/button/style';
+import '@gio-design/components/es/components/tooltip/style/index.css';
