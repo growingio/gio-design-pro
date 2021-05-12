d1d7ab5430d86f26b900f8ef5cf89913538b8d98
itiiss <kurarin@outlook.com>
2021-05-12 16:17:22 +0800

    feat(filter-picker): 增加参数placement (#244)
    Co-authored-by: ZhaoChen <ittisennsinn@gmail.com>

--- src/filter-picker/interfaces.ts
+++ src/filter-picker/interfaces.ts
@@ -14,6 +14,7 @@ export interface FilterPickerProps extends Pick<PropertyPickerProps, 'fetchDetai
   hasVisible?: boolean;
   visible?: boolean;
   onVisibleChange?: (v: boolean) => void;
+  placement?: 'bottomLeft' | 'bottom' | 'bottomRight';
 }
 export type attributeValue = 'string' | 'int' | 'date';
 
