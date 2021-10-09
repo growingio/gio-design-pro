8a4834b63fc6f199b63c0632a40c7dd6f2fccad7
WORLDI <66404428+WORLDI@users.noreply.github.com>
2021-10-09 14:08:56 +0800

    fix(event-picker): fix placeholder prop (#434)

--- src/event-selector/interfaces.ts
+++ src/event-selector/interfaces.ts
@@ -3,4 +3,8 @@ import { SelectorProps } from '../selector';
 
 export interface EventSelectorProps extends EventPickerProps, Omit<SelectorProps, 'dropdownRender' | 'valueRender'> {
   showValueIcon?: boolean;
+  /**
+   * 自定义 eventPicker 的 placeholder
+   */
+  pickerPlaceholder?: string;
 }
